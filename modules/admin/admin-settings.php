d42078ac12101046bbf03773fcb409e56c8e03eb
my-code-library <mrscottwebber@gmail.com>
2026-01-04 14:35:32 -0800

    Update admin-settings.php

--- modules/admin/admin-settings.php
+++ modules/admin/admin-settings.php
@@ -20,43 +20,40 @@ class PJ_Settings_Page {
         );
     }
 
-    public static function register_settings() {
+register_setting('pj_settings_group', 'pj_turnstile_site_key');
+register_setting('pj_settings_group', 'pj_turnstile_secret_key');
 
-        // Register Turnstile keys
-        register_setting('pj_settings_group', 'pj_turnstile_site_key');
-        register_setting('pj_settings_group', 'pj_turnstile_secret_key');
+add_settings_section(
+    'pj_turnstile_section',
+    'Cloudflare Turnstile',
+    function() {
+        echo '<p>Protect login and registration with Cloudflare Turnstile.</p>';
+    },
+    'pj-settings'
+);
 
-        add_settings_section(
-            'pj_turnstile_section',
-            'Cloudflare Turnstile',
-            function() {
-                echo '<p>Protect login and registration with Cloudflare Turnstile.</p>';
-            },
-            'pj-settings'
-        );
+add_settings_field(
+    'pj_turnstile_site_key',
+    'Turnstile Site Key',
+    function() {
+        $value = esc_attr(get_option('pj_turnstile_site_key', ''));
+        echo '<input type="text" name="pj_turnstile_site_key" value="' . $value . '" class="regular-text">';
+    },
+    'pj-settings',
+    'pj_turnstile_section'
+);
 
-        add_settings_field(
-            'pj_turnstile_site_key',
-            'Turnstile Site Key',
-            function() {
-                $value = esc_attr(get_option('pj_turnstile_site_key', ''));
-                echo '<input type="text" name="pj_turnstile_site_key" value="' . $value . '" class="regular-text">';
-            },
-            'pj-settings',
-            'pj_turnstile_section'
-        );
+add_settings_field(
+    'pj_turnstile_secret_key',
+    'Turnstile Secret Key',
+    function() {
+        $value = esc_attr(get_option('pj_turnstile_secret_key', ''));
+        echo '<input type="text" name="pj_turnstile_secret_key" value="' . $value . '" class="regular-text">';
+    },
+    'pj-settings',
+    'pj_turnstile_section'
+);
 
-        add_settings_field(
-            'pj_turnstile_secret_key',
-            'Turnstile Secret Key',
-            function() {
-                $value = esc_attr(get_option('pj_turnstile_secret_key', ''));
-                echo '<input type="text" name="pj_turnstile_secret_key" value="' . $value . '" class="regular-text">';
-            },
-            'pj-settings',
-            'pj_turnstile_section'
-        );
-    }
 
     public static function render_page() {
         ?>
