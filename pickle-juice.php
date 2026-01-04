3ba5869b3a601efd45027f2e25e4b3444379d199
my-code-library <mrscottwebber@gmail.com>
2026-01-04 14:00:31 -0800

    Update pickle-juice.php

--- pickle-juice.php
+++ pickle-juice.php
@@ -16,5 +16,13 @@ if (!defined('ABSPATH')) {
 // Include the module loader
 require_once plugin_dir_path(__FILE__) . 'includes/module-loader.php';
 
+// Include admin settings page
+require_once plugin_dir_pat(__FILE__) . 'includes/admin-settings.php';
 
-
+// Turnstile support
+function pj_get_turnstile_keys() {
+    return [
+        'site'   => get_option('pj_turnstile_site_key', ''),
+        'secret' => get_option('pj_turnstile_secret_key', ''),
+    ];
+}
