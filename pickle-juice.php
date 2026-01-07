fa6fbf05f7bfe9445a978cc110dd035aed98a952
my-code-library <mrscottwebber@gmail.com>
2026-01-06 22:27:57 -0800

    Update pickle-juice.php

--- pickle-juice.php
+++ pickle-juice.php
@@ -3,7 +3,7 @@
  * Plugin Name: Pickle Juice
  * Description: Email only (no usernames) registration and login, Google & Microsoft analytics/webmaster tools, custom login logo, Cloudflare Turnstile support, magic login link support, custom login url support
  * Author: Pickle Juice
- * Version: 1.1.1
+ * Version: 1.1.2
  * Text Domain: pickle-juice
  * Domain Path: /languages
  */
@@ -15,3 +15,4 @@ if (!defined('ABSPATH')) {
 
 // Include the module loader
 require_once plugin_dir_path(__FILE__) . 'includes/module-loader.php';
+
