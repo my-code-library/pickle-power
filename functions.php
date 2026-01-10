a612d7e154f1af17e8c9d1cc422380ee511e3b9f
my-code-library <mrscottwebber@gmail.com>
2026-01-10 11:41:33 -0800

    Update and rename picklejuice.php to functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Pickle Juice
+ * Plugin Name: Pickle Power
  * Description: Email only (no usernames) registration and login, Google & Microsoft analytics/webmaster tools, custom login logo, Cloudflare Turnstile support, magic login link support, custom login url support, Wordpress debranding support
  * Author: Pickle Juice
  * Version: 1.1.4
@@ -23,6 +23,7 @@ add_filter('plugin_action_links_' . plugin_basename(__FILE__), function($links) 
 
 // Include the module loader
 require_once plugin_dir_path(__FILE__) . 'includes/module-loader.php';
+
 
 
 
