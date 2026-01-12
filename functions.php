ffaf25ceb4ecc8ae3894e1fdca5ec28c52a76493
my-code-library <mrscottwebber@gmail.com>
2026-01-11 16:49:28 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,6 +8,25 @@
  * Domain Path: /languages
  */
 
+/**
+Pickle Power – A modular WordPress plugin for branded login, security, and admin UX.
+Copyright (C) 2026 Gold Coast Music/Pickle Juice
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 // No direct access
 if (!defined('ABSPATH')) {
     exit;
@@ -23,6 +42,7 @@ add_filter('plugin_action_links_' . plugin_basename(__FILE__), function($links) 
 
 // Include the module loader
 require_once plugin_dir_path(__FILE__) . 'includes/module-loader.php';
+
 
 
 
