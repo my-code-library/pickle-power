5b0cad6940f62f59ece9dbd1168682a42cac9306
Code Master General <252930943+my-code-library@users.noreply.github.com>
2026-01-27 09:07:43 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,22 +9,21 @@
  */
 
 /**
-Pickle Power – A modular WordPress plugin for branded login, security, and admin UX.
-Copyright (C) 2026 Gold Coast Music/Pickle Juice
+    Pickle Power – A modular WordPress plugin for branded login, security, and admin UX.
+    Copyright (C) 2026 Gold Coast Music/Pickle Juice
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 // No direct access
@@ -42,6 +41,7 @@ add_filter('plugin_action_links_' . plugin_basename(__FILE__), function($links) 
 
 // Include the module loader
 require_once plugin_dir_path(__FILE__) . 'includes/module-loader.php';
+
 
 
 
